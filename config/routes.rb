437013ac59bccffb84c8c5f27c1b69184c4499ff
alihass3 <hasan.ali.mnpk202@gmail.com>
2017-07-24 18:12:19 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+#in this file you create routes for the rails application
+
+
+
+Rails.application.routes.draw do
+  get 'pictures' => 'pictures#index'
+
+  post 'pictures' => 'pictures#create'
+
+  get 'pictures/new' => 'pictures#new'
+   
+  get 'pictures/:id' => 'pictures#show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
