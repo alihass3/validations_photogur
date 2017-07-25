06751c85a4a4d0d235c2caa06c50a3cccc93cbfa
alihass3 <hasan.ali.mnpk202@gmail.com>
2017-07-25 03:04:41 -0400

    validations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,13 @@
 class Picture < ApplicationRecord
-  
+  validates :artist, presence: true,on: :save
+  validates :title, length: { in: 6..20 },on: :save
+  validates :url, presence: true, uniqueness: true,on: :save
 end
+
+
+#validates :password, length: { in: 6..20 }
+#validates :email, uniqueness: true, on: :create
+#validates :email, uniqueness: true
+#validates :password, length: { in: 6..20 }
+#validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
+#validates :email, presence: true, email: true
